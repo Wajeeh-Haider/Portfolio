56257bf16214ab551b6aaa78f73fec4d17296922
Wajeeh-Haider <swajeeh03@gmail.com>
2022-10-31 01:29:35 -0700

    code refactor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Header, Skills, Testimonials, Footer, About, Work } from "./container";
 import Navbar from "./components/Navbar/Navbar";
 import "./App.scss";
-
 const App = () => {
   return (
     <>
